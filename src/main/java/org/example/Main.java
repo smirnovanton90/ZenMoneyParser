27d87b2c9b402a5cd63b0b04c5c6fd4499b04390
smirnovanton90 <smirnovanton90@gmail.com>
2022-11-05 13:48:17 +0300

    Added file parser

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,28 @@
 package org.example;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        File file = new File("C:" + File.separator + "Users" + File.separator + "smirn" + File.separator + "Desktop" + File.separator + "Транзакции.mhtml");
+
+        try {
+            Document document = Jsoup.parse(file);
+//            var titleElement = document.body();
+//            System.out.println(titleElement.text());
+
+            Elements elements = document.select("h1");
+            for (var element : elements) {
+                System.out.println(element.text());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
